55893a7ff217b5bec1831c7be8de47cdff53f634
Dung2781993@gmail.com <Dung2781993@gmail.com>
2022-01-19 03:12:02 +1100

    Update Login component

---
+++ src/components/Signup.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="vue-tempalte">
+    <form>
+      <h3>Sign Up</h3>
+
+      <div class="form-group">
+        <label>Full Name</label>
+        <input type="text" class="form-control form-control-lg" />
+      </div>
+
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control form-control-lg" />
+      </div>
+
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control form-control-lg" />
+      </div>
+
+      <div class="form-group">
+        <button type="button" class="btn btn-info btn-block btn-submit">
+          Sign Up
+        </button>
+      </div>
+
+      <p class="forgot-password text-right">
+        Already registered
+        <router-link :to="{ name: 'login' }">sign in?</router-link>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
