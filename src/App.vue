d4bad8482cba79b97872b358e390feb25017663e
Dung2781993@gmail.com <Dung2781993@gmail.com>
2022-01-20 16:07:21 +1100

    Update Home page

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,41 @@
 <template>
-  <div class="vue-tempalte">
-    <!-- Navigation -->
-    <nav
-      class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top"
-    >
-      <div class="container">
-        <a
-          class="navbar-brand float-left"
-          href="https://www.loopit.co/"
-          target="_blank"
-        >
-          Loopit
-        </a>
-        <ul class="nav navbar-nav flex-row float-right">
-          <li class="nav-item" v-if="!currentUser">
-            <router-link to="/" class="nav-link">
-              <font-awesome-icon class="icon-auth" icon="sign-in-alt" />Login
-            </router-link>
-          </li>
-          <li class="nav-item" v-if="!currentUser">
-            <router-link class="btn btn-outline-primary" to="/signup">
-              <font-awesome-icon class="icon-auth" icon="user-plus" />Sign Up
-            </router-link>
-          </li>
-          <li class="nav-item" v-if="currentUser">
-            <a class="nav-link" href @click.prevent="logOut">
-              <font-awesome-icon icon="sign-out-alt" />LogOut
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  <div class="App">
+    <div class="vue-tempalte">
+      <!-- Navigation -->
+      <nav
+        class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top"
+      >
+        <div class="container">
+          <a class="navbar-brand float-left" href="/home"> Loopit </a>
+          <ul class="nav navbar-nav flex-row float-right">
+            <li class="nav-item" v-if="!currentUser">
+              <router-link to="/login" class="nav-link">
+                <font-awesome-icon class="icon-auth" icon="sign-in-alt" />Login
+              </router-link>
+            </li>
+            <li class="nav-item" v-if="!currentUser">
+              <router-link class="btn btn-outline-primary" to="/signup">
+                <font-awesome-icon class="icon-auth" icon="user-plus" />Sign Up
+              </router-link>
+            </li>
 
-    <!-- Main -->
-    <div class="App">
+            <li class="nav-item" v-if="currentUser">
+              <router-link to="/profile" class="nav-link">
+                <font-awesome-icon class="icon-auth" icon="user" />
+                {{ currentUser.user.name }}
+              </router-link>
+            </li>
+            <li class="nav-item" v-if="currentUser">
+              <a class="nav-link" href @click.prevent="logOut">
+                <font-awesome-icon class="icon-auth" icon="sign-out-alt" />LogOut
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <!-- Main -->
+
       <div class="ui text container" style="margin-top: 7em">
         <router-view />
       </div>
