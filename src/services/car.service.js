6e785aa0b6ab0a6f8ec9a4b115cde8841db28de0
Dung2781993@gmail.com <Dung2781993@gmail.com>
2022-01-23 22:05:20 +1100

    Update API end point

--- src/services/car.service.js
+++ src/services/car.service.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-const API_URL = "http://127.0.0.1:8000/api/car/";
+
+import apiService from "./api.service";
+
+const API_URL = apiService() + "api/car/";
+
 class CarService {
   getHeader() {
     return authHeader();
